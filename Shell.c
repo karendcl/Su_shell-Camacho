e8ad957d632057a5256dc7d85f17955966c124c9
karendcl <89109943+karendcl@users.noreply.github.com>
2023-04-16 15:32:22 -0400

    Fixing cd

--- Shell.c
+++ Shell.c
@@ -10,6 +10,38 @@
 
 #define	MAX_SIZE_CMD	256
 #define	MAX_SIZE_ARG	16
+#define TRUE 1
+#define FALSE !TRUE
+
+// Shell pid, pgid, terminal modes
+static pid_t NTL_PID;
+static pid_t NTL_PGID;
+static int NTL_IS_INTERACTIVE;
+static struct termios NTL_TMODES;
+
+static char* currentDirectory;
+extern char** environ;
+
+struct sigaction act_child;
+struct sigaction act_int;
+
+int no_reprint_prmpt;
+int isPiping = 0;
+
+pid_t pid;
+
+
+// signal handler for SIGCHLD */
+void signalHandler_child(int p);
+// signal handler for SIGINT
+void signalHandler_int(int p);
+
+int changeDirectory(char * args[]);
+
+
+
+
+
 
 char cmd[MAX_SIZE_CMD];				
 // string holder for the command
